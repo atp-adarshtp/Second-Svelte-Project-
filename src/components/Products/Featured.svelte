453063a89021a3b01badcb18d023b7519305ad65
Adarsh TP <atp@dewdrive.com>
2024-03-28 12:53:52 +0530

    FEATURED PRODUCTS

---
+++ src/components/Products/Featured.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let title = "";
+    import products from "../../stores/dlefaultProducts";
+    import Product from "./Product.svelte";
+    import Loading from "../Loading.svelte";
+    $:featured = $products.filter(item => item.featured === true)
+</script>
+
+{#if featured.length === 0}
+<Loading />
+{:else}
+<section class="section">
+    <h2 class="section-title">{title}</h2>
+    <div class="products-center">
+        {#each featured as product (product.id)}
+            <Product {product} />
+        {/each}
+    </div>
+</section>
+{/if}
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,7 +1,9 @@
 <script>
     import Hero from "../components/Hero.svelte";
     import {link} from "svelte-routing";
+    import Featured from "../components/Products/Featured.svelte"
 </script>
 <Hero>
     <a href='/products' class="btn btn-primary btn-hero" use:link>shop now</a>
 </Hero>
+<Featured title="featured products"/>
