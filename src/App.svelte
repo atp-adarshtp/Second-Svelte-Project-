ea1db52037047cfb31bd197d5731a0c990a12958
Adarsh TP <atp@dewdrive.com>
2024-03-30 17:10:18 +0530

    Small Navbar
    273

--- src/App.svelte
+++ src/App.svelte
@@ -5,18 +5,17 @@
 	import ProductTemplats from "./pages/ProductTemplate.svelte";
 	import Checkout from "./pages/Checkout.svelte";
 	import Home from "./pages/Home.svelte";
+
 	import About from "./pages/About.svelte";
 
 	// Import the necessary routing components
 	import { Router, Route, Link } from "svelte-routing";
+	// components
+	import Navbar from "./components/Navbar/Navbar.svelte";
 </script>
 
 <Router>
-	<nav class="navbar">
-		<Link to="/">Home</Link>
-		<Link to="/about">About</Link>
-		<Link to="/products">Products</Link>
-	</nav>
+	<Navbar />
 	<Route path="/" component={Home} />
 	<Route path="/about" component={About} />
 	<Route path="/login" component={Login} />
