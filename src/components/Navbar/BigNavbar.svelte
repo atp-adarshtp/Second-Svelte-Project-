05df1f8cbe31c79d176025d229adfee011d7d700
Adarsh TP <atp@dewdrive.com>
2024-03-30 18:07:42 +0530

    Links
    276

--- src/components/Navbar/BigNavbar.svelte
+++ src/components/Navbar/BigNavbar.svelte
@@ -1,19 +1,31 @@
 <script>
     import CartButton from "../Cart/CartButton.svelte";
-    import {link} from "svelte-routing";
+    import { link } from "svelte-routing";
+    import links from "../../constants/links";
+    // import { home } from "nodemon/lib/utils";
+    // import Product from "../Products/Product.svelte";
 </script>
 
 <nav class="navbar">
     <div class="nav-container">
-<div class="nav.center">
-<!-- nav links -->
-<p>nav  links</p>
-<!-- nav logo -->
-<a href="/" use:link class="nav-logo big-logo">
-    <img src="/assets/images/logo.svg" class="logo" alt="razors logo" /></a>
+        <div class="nav.center">
+            <!-- nav links -->
+            <ul class="nav-links">
+           {#each links as navLink}
+            <li><a href="{navLink.url}" use:link>{navLink.text}</a></li>
+           {/each}
+        </ul>
+            <!-- nav logo -->
+            <a href="/" use:link class="nav-logo big-logo">
+                <img
+                    src="/assets/images/logo.svg"
+                    class="logo"
+                    alt="razors logo"
+                /></a
+            >
+        </div>
+    </div>
     <div class="nav-aside">
         <CartButton />
-    </div>
-</div>
     </div>
 </nav>
---
+++ src/constants/links.js
@@ -0,0 +1,5 @@
+export default [
+    {text:'home',url:'/'},
+    {text:'product',url:'/product'},
+    {text:'about',url:'/about'},
+]
