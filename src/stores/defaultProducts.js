2ff23fc730683c78a37a2665301657525d696c81
Adarsh TP <atp@dewdrive.com>
2024-03-30 13:47:15 +0530

    Single product page
    271

--- src/stores/defaultProducts.js
+++ src/stores/defaultProducts.js
@@ -1,15 +1,17 @@
-import { derived, writable } from "svelte/store";
-import localProducts from '../defaultProducts';
-const store = writable(flattenProducts([...localProducts]));
+import { writable, derived } from "svelte/store";
+import localProducts from '../localProducts';
 
+const store = writable(flattenProducts([...localProducts]));
 
 function flattenProducts(data) {
     return data.map(item => {
         let image = item.image.url;
-        return { ...item, image }
-    })
+        return { ...item, image };
+    });
 }
-export const featuredStore = derived(store,($featured)=>{
+
+export const featuredStore = derived(store, ($featured) => {
     return $featured.filter(item => item.featured === true);
-})
+});
+
 export default store;
