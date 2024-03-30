988e568b5c531faa9446946e46f22c2135e70ac7
Adarsh TP <atp@dewdrive.com>
2024-03-30 14:31:05 +0530

    Svelte:head element
    272

--- src/pages/ProductTemplate.svelte
+++ src/pages/ProductTemplate.svelte
@@ -6,29 +6,36 @@
     $: product = $products.find((item) => item.id == parseInt(id));
 </script>
 
+<svelte:head>
+    <title>{!product ? "single product" : product.title}</title>
+</svelte:head>
+
 {#if !product}
-<Loading />
+    <Loading />
 {:else}
-<section class="single-product">
-    <!-- back to products -->
-    <a href="/products" use:link class="btn btn-primary">back to products</a>
-    <!-- single product container -->
-    <div class="single-product-container">
-        <article class="single-product-image">
-            <img src={product.image} alt={product.title}>
-        </article>
-        <article>
-            <h1>{product.title}</h1>
-            <h2>${product.price}</h2>
-            <p>{product.description}</p>
-            <button class="btn btn-primary btn-block" on:click="{()=>{
-                console.log('add to cart');
-                
-            }}">
-                add to cart
-            </button>
-        </article>
-    </div>
-</section>
+    <section class="single-product">
+        <!-- back to products -->
+        <a href="/products" use:link class="btn btn-primary">back to products</a
+        >
+        <!-- single product container -->
+        <div class="single-product-container">
+            <article class="single-product-image">
+                <img src={product.image} alt={product.title} />
+            </article>
+            <article>
+                <h1>{product.title}</h1>
+                <h2>${product.price}</h2>
+                <p>{product.description}</p>
+                <button
+                    class="btn btn-primary btn-block"
+                    on:click={() => {
+                        console.log("add to cart");
+                    }}
+                >
+                    add to cart
+                </button>
+            </article>
+        </div>
+    </section>
 {/if}
 <h1>Welcome to Product Template page with id:{id}</h1>
