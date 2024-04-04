e96f54ac8e37cd3f444b18b95cfa07104a77e17d
Adarsh TP <atp@dewdrive.com>
2024-04-04 14:39:27 +0530

    Toggle navbars
    277

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,7 +1,14 @@
 <script>
     import SmallNavbar from "./SmallNavbar.svelte";
     import BigNavbar from "./BigNavbar.svelte";
+    let screenWidth;
+
 </script>
 
-<SmallNavbar />
+<svelte:window bind:innerWidth={screenWidth} />
+{screenWidth}
+{#if screenWidth > 992}
 <BigNavbar />
+{:else}
+<SmallNavbar />
+{/if}
