0277a4fed5524d0ff2badce1f8836d2bb04a130b
Adarsh TP <atp@dewdrive.com>
2024-04-04 19:14:00 +0530

    Setup Close Sidebar Function
    281

--- src/components/Navbar/Sidebar.svelte
+++ src/components/Navbar/Sidebar.svelte
@@ -1,12 +1,15 @@
 <script>
     import links from "../../constants/links";
     import { link } from "svelte-routing";
+    import globalStore from "../../stores/globalStore";
 </script>
 
 <div class="sidebar-container">
     <div class="sidebar">
         <div class="sidebar-header">
-            <button class="btn-close">
+            <button class="btn-close" on:click="{()=>{
+                globalStore.toggleItem('sidebar',false)
+            }}">
                 <i class="fas fa-window-close" />
             </button>
         </div>
@@ -22,7 +25,7 @@
                         href={sideLink.url}
                         use:link
                         on:click={() => {
-                            console.log("sidebar closed");
+                                 globalStore.toggleItem('sidebar',false)
                         }}>{sideLink.text}</a
                     >
                 </li>
