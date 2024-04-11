936ddffaf3ec348511461e32b82a8e823c8e1c0e
Adarsh TP <atp@dewdrive.com>
2024-04-11 11:54:43 +0530

    Cart Total
    289

--- src/components/Cart/ItemsList.svelte
+++ src/components/Cart/ItemsList.svelte
@@ -1,6 +1,6 @@
 <script>
     import Item from "./Items.svelte";
-    import cart from "../../stores/cart";
+    import cart, { cartTotal } from "../../stores/cart";
     import { fly } from "svelte/transition";
     import { flip } from "svelte/animate";
 </script>
@@ -8,9 +8,16 @@
 <section class="cart-items">
     <article>
         {#each $cart as cartItems, index (cartItems.id)}
-        <div in:fly={{ delay: (index + 1) * 500, x: 100 }} out:fly={{ x: -100 }} animate:flip />
+            <div
+                in:fly={{ delay: (index + 1) * 500, x: 100 }}
+                out:fly={{ x: -100 }}
+                animate:flip
+            >
+                <Item {...cartItems} />
+            </div>
         {:else}
             <h2 class="empty-cart">is currently empty...</h2>
         {/each}
     </article>
+    <h3 class="cart-total">total : ${cartTotal}</h3>
 </section>
